bf692b7d51295fc4fb8f6b81e1da5f8c5fdfda32
AllredW <allrewil@gmail.com>
2024-08-21 19:33:35 -0400

    added seed files, populated with test data

---
+++ seeds/thought-seed.js
@@ -0,0 +1,115 @@
+const seedThoughts = [
+    {
+      thoughtText: "This is a sample thought about something interesting.",
+      username: "Bob",
+      reactions: [
+        {
+          reactionBody: "❤️",
+          username: "Mark",
+        },
+        {
+          reactionBody: "👍",
+          username: "Sally",
+        },
+      ],
+    },
+    {
+      thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      username: "Mark",
+      reactions: [
+        {
+          reactionBody: "❤️",
+          username: "Bob",
+        },
+        {
+          reactionBody: "👍",
+          username: "Sally",
+        },
+      ],
+    },
+    {
+      thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      username: "Sally",
+      reactions: [
+        {
+          reactionBody: "❤️",
+          username: "Bob",
+        },
+        {
+          reactionBody: "👍",
+          username: "Mark",
+        },
+      ],
+    },
+    {
+      thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      username: "Rachel",
+      reactions: [
+        {
+          reactionBody: "❤️",
+          username: "Bob",
+        },
+        {
+          reactionBody: "👍",
+          username: "Mark",
+        },
+      ],
+    },
+    {
+      thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      username: "Bob",
+      reactions: [
+        {
+          reactionBody: "❤️",
+          username: "Mark",
+        },
+        {
+          reactionBody: "👍",
+          username: "Sally",
+        },
+      ],
+    },
+    {
+      thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      username: "Mark",
+      reactions: [
+        {
+          reactionBody: "❤️",
+          username: "Bob",
+        },
+        {
+          reactionBody: "👍",
+          username: "Sally",
+        },
+      ],
+    },
+    {
+      thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      username: "Sally",
+      reactions: [
+        {
+          reactionBody: "❤️",
+          username: "Bob",
+        },
+        {
+          reactionBody: "👍",
+          username: "Mark",
+        },
+      ],
+    },
+    {
+      thoughtText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      username: "Rachel",
+      reactions: [
+        {
+          reactionBody: "❤️",
+          username: "Bob",
+        },
+        {
+          reactionBody: "👍",
+          username: "Mark",
+        },
+      ],
+    },
+  ];
+  module.exports = seedThoughts;
